6bcb015aa61750affae9ee7ece053a650da85973
Nathyara <nathyaracorraledrosa@gmail.com>
2026-04-23 10:39:41 +0200

    Cambios código

--- src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
+++ src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
@@ -12,19 +12,19 @@ import es.dam.codeoptimization.PlayerStats;
 public class FantasyCalculator {
 
     // Method to calculate the points
-    public static int calcP(PlayerStats s) {
+    public static int calcularFantasyPoints(PlayerStats stats) {
         int fantasyPoints = 0; 
         int points = 0;
         
-        int minutes = s.minutes;
-        int goals = s.goals;
-        int assists = s.assists;
-        boolean yellowCard = s.yellowCard;
-        boolean redCard = s.redCard;
-        int saves = s.saves;
-        int goalsAgainst = s.goalsAgainst;
-        char matchResult = s.matchResult;
-        String position = s.position;
+        int minutes = stats.minutes;
+        int goals = stats.goals;
+        int assists = stats.assists;
+        boolean yellowCard = stats.yellowCard;
+        boolean redCard = stats.redCard;
+        int saves = stats.saves;
+        int goalsAgainst = stats.goalsAgainst;
+        char matchResult = stats.matchResult;
+        String position = stats.position;
 
         // --- GOALKEEPER LOGIC ---
         if (position.equals("PORTERO")) {
@@ -43,83 +43,83 @@ public class FantasyCalculator {
             // 1 point per save
             fantasyPoints += saves; 
             
-            if (goalsAgainst == 0) {
-                fantasyPoints += 5; 
-            } else if (goalsAgainst == 1) {
-                fantasyPoints += 3;
-            } else if (goalsAgainst == 2) {
-                fantasyPoints += 1;
+            switch (goalsAgainst) {
+                case 0 -> fantasyPoints += 5;
+                case 1 -> fantasyPoints += 3;
+                case 2 -> fantasyPoints += 1;
+                default -> {
+                }
             }
 
-            if (yellowCard == true) fantasyPoints = fantasyPoints - 3; 
-            if (redCard == true) fantasyPoints = fantasyPoints - 5;
+            if (yellowCard) fantasyPoints -= 3; 
+            if (redCard) fantasyPoints -= 5;
             
             if (matchResult == 'G') {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             } else if (matchResult == 'E') {
-                fantasyPoints = fantasyPoints + 2;
+                fantasyPoints += 2;
             }
 
         // --- DEFENDER LOGIC ---
         } else if (position.equals("DEFENSA")) {
             if (minutes > 0 && minutes < 60) {
-                fantasyPoints = fantasyPoints + 3;
+                fantasyPoints += 3;
             } else if (minutes >= 60) {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             }
 
             for (int i = 0; i < goals; i++) {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             }
 
             fantasyPoints = fantasyPoints + (assists * 6);
 
-            if (goalsAgainst == 0) {
-                fantasyPoints = fantasyPoints + 5; 
-            } else if (goalsAgainst == 1) {
-                fantasyPoints = fantasyPoints + 3;
-            } else if (goalsAgainst == 2) {
-                fantasyPoints = fantasyPoints + 1;
+            switch (goalsAgainst) {
+                case 0 -> fantasyPoints = fantasyPoints + 5;
+                case 1 -> fantasyPoints = fantasyPoints + 3;
+                case 2 -> fantasyPoints = fantasyPoints + 1;
+                default -> {
+                }
             }
 
-            if (yellowCard == true) fantasyPoints = fantasyPoints - 3;
-            if (redCard == true) fantasyPoints = fantasyPoints - 5;
+            if (yellowCard) fantasyPoints -= 3;
+            if (redCard) fantasyPoints -= 5;
             
             if (matchResult == 'G') {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             } else if (matchResult == 'E') {
-                fantasyPoints = fantasyPoints + 2;
+                fantasyPoints += 2;
             }
 
         // --- MIDFIELDER LOGIC ---
         } else if (position.equals("MEDIO")) {
             if (minutes > 0 && minutes < 60) {
-                fantasyPoints = fantasyPoints + 3;
+                fantasyPoints += 3;
             } else if (minutes >= 60) {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             }
 
             for (int i = 0; i < goals; i++) {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             }
 
             fantasyPoints = fantasyPoints + (assists * 6);
 
-            if (yellowCard == true) fantasyPoints = fantasyPoints - 3;
-            if (redCard == true) fantasyPoints = fantasyPoints - 5;
+            if (yellowCard) fantasyPoints -= 3;
+            if (redCard) fantasyPoints -= 5;
             
             if (matchResult == 'G') {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             } else if (matchResult == 'E') {
-                fantasyPoints = fantasyPoints + 2;
+                fantasyPoints += 2;
             }
 
         // --- FORWARD LOGIC ---
         } else if (position.equals("DELANTERO")) {
             if (minutes > 0 && minutes < 60) {
-                fantasyPoints = fantasyPoints + 3;
+                fantasyPoints += 3;
             } else if (minutes >= 60) {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             }
 
             for (int i = 0; i < goals; i++) {
@@ -128,13 +128,13 @@ public class FantasyCalculator {
 
             fantasyPoints = fantasyPoints + (assists * 5);
 
-            if (yellowCard == true) fantasyPoints = fantasyPoints - 3;
-            if (redCard == true) fantasyPoints = fantasyPoints - 5;
+            if (yellowCard) fantasyPoints -= 3;
+            if (redCard) fantasyPoints -= 5;
             
             if (matchResult == 'G') {
-                fantasyPoints = fantasyPoints + 5;
+                fantasyPoints += 5;
             } else if (matchResult == 'E') {
-                fantasyPoints = fantasyPoints + 2;
+                fantasyPoints += 2;
             }
         }
 
